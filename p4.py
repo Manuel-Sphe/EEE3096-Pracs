cbfcfd8d9a3c42177c9e38e5d4e5d0b1593ff220
mahluli <42044971+mahluli@users.noreply.github.com>
2021-09-19 09:28:39 +0200

    Update p4.py

--- p4.py
+++ p4.py
@@ -13,13 +13,13 @@ LED_value = [11, 13, 15]
 LED_accuracy = 32
 btn_submit = 16
 btn_increase = 18
-buzzer = None
+buzzer = 33
 eeprom = ES2EEPROMUtils.ES2EEPROM()
 
 # gloabal var
+user = ""
 guesses = 0
-current_scores = [["ChB", 5], ["Ada", 7], ["LSu", 4], ["EEE", 8]]
-
+scores = [["ChB", 5], ["Ada", 7], ["LSu", 4], ["EEE", 8]]
 
 current_val = 0
 ran_val =0
@@ -114,27 +114,15 @@ def setup():
     # buttons
     GPIO.setup(btn_submit,GPIO.IN,pull_up_down = GPIO.PUD_UP)
 
-
     GPIO.setup(btn_increase,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
     # Setup PWM channels
 
-    pi_pwm = GPIO.PWM(LED_accuracy,1000)
+    pi_pwm = GPIO.PWM(LED_accuracy,100)
     pi_pwm.start(0)
-
+    #setup buzzer
+    GPIO.setup(buzzer,GPIO.OUT)
     GPIO.setwarnings(False)	
-   ## while True:
-      ##  for duty in range(101):
-           ## pi_pwm.ChangeDutyCycle(duty)
-            ##sleep(0.01)
-       ## sleep(0.5)
-
-        #for duty in range(100,-1,-1):
-          #  pi_pwm.ChangeDutyCycle(duty)
-           # sleep(0.01)
-        #sleep(0.5)
-
-            
 
     # Setup debouncing and callbacks
 
@@ -142,17 +130,17 @@ def setup():
 
     GPIO.add_event_detect(btn_submit,GPIO.FALLING,callback = btn_guess_pressed,bouncetime = 300)
     
-
-
-
     pass
 
 
 # Load high scores
 def fetch_scores():
     # get however many scores there are
-    scores = ES2EEPROMUtils.read_block()
-    score_count = len(scores)
+    score_count = None
+    
+    #scores = ES2EEPROMUtils.read_block()
+    score_count = eeprom.read_byte(0)
+    scores = eeprom.read_block(1,score_count*4)
     # Get the scores
     char_score = [] 
     for i in range(score_count):
@@ -170,27 +158,26 @@ def fetch_scores():
 # Save high scores
 def save_scores():
     # fetch scores
-    num_scores , scores = fetch_scores()
+    score_count = eeprom.read_byte(0)
+    scores = eeprom.read_block(1, score_count*4)
+    #num_scores , scores = fetch_scores()
     # include new score 
-    # username = input("Enter Name: \n")
-
-    current_scores.sort(key=lambda x: x[1])
-    for i, score in enumerate(current_scores):
-        data_to_write = []
-        # get the string
-        for letter in score[0]:
-            data_to_write.append(ord(letter))
-        data_to_write.append(score[1])
-        self.write_block(num_scores+1, data_to_write)
-
+    data = []
+    for i in range(score_count):
+        data.append(scores[slice(i*4, i*4+4)])
+    score = list(user)
+    score = [ord(i) for i in score]
+    score.append(guesses)
+    data.append(score)
     
-   
-    
-   
     # update total amount of scores
+    score_count = score_count+1
     
     # write new scores
-
+    data.sort(key=lambda x: x[3])
+    data = [b for c in data for b in c]
+    eeprom.write_block(1,data)
+    eeprom.write_block(0,[score_count])
     pass
 
 
@@ -206,6 +193,21 @@ def btn_increase_pressed(channel):
     print("Increased pressed(B0)!!!")
     # You can choose to have a global variable store the user's current guess, 
     # or just pull the value off the LEDs when a user makes a guess
+    current_val = current_val+1
+    bin_var = bin(current_val).replace("0b","")
+    if current_val==0 or current_val ==1:
+        bin_var = "00"+bin_var
+    if(current_val==3 or current_val==2):
+        bin_var = "0"+bin_var
+
+    ls = list(bin_var)
+
+    for i in range(3):
+        val = int(ls[i])
+        if val == 0:
+            GPIO.output(LED_value[i], GPIO.LOW)
+        else:
+            GPIO.output(LED_value[i], GPIO.HIGH)        
     pass
 
 
@@ -233,6 +235,14 @@ def accuracy_leds():
     # - The % brightness should be directly proportional to the % "closeness"
     # - For example if the answer is 6 and a user guesses 4, the brightness should be at 4/6*100 = 66%
     # - If they guessed 7, the brightness would be at ((8-7)/(8-6)*100 = 50%
+    if(ran_val>current_val):
+        duty = (current_val/ran_val)*100
+        pi_pwm.ChangeDutyCycle(duty)
+    if(ran_val<current_val:
+        duty = (8-current_val)/(8-ran_val)*100
+        pi_pwm.ChangeDutyCycle(duty)
+    else:
+       pi_pwm.ChangeDutyCycle(100)
     pass
 
 # Sound Buzzer
@@ -241,8 +251,27 @@ def trigger_buzzer():
     # While we want the brightness of the LED to change(duty cycle), we want the frequency of the buzzer to change
     # The buzzer duty cycle should be left at 50%
     # If the user is off by an absolute value of 3, the buzzer should sound once every second
+    difference = abs(ran_val - current_val)
+    if(difference == 3):
+        while true:
+            GPIO.output(buzzer,GPIO.HIGH)
+            sleep(0.5) # Delay in seconds
+            GPIO.output(buzzer,GPIO.LOW)
+            sleep(0.5)        
     # If the user is off by an absolute value of 2, the buzzer should sound twice every second
+    if(difference == 2):
+        while true:
+            GPIO.output(buzzer,GPIO.HIGH)
+            sleep(0.25) # Delay in seconds
+            GPIO.output(buzzer,GPIO.LOW)
+            sleep(0.25)   
     # If the user is off by an absolute value of 1, the buzzer should sound 4 times a second
+    if(difference == 1):
+        while true:
+            GPIO.output(buzzer,GPIO.HIGH)
+            sleep(0.125) # Delay in seconds
+            GPIO.output(buzzer,GPIO.LOW)
+            sleep(0.125)   
     pass
 
 
